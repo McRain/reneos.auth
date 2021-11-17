e2fffe077fafca8d4c6e410c737ae68e3a62019c
McRain <8288917+McRain@users.noreply.github.com>
2021-11-17 15:27:28 +0400

    update

--- workers/extget.js
+++ workers/extget.js
@@ -1,8 +1,5 @@
 import request from "../request.js"
 
-/**
- * If the data is a string, it requests data from the external service
- */
 export default async (options,data,req,res) => {
 	if (typeof data === "string") {
 		return await request({
--- workers/fieldsexists.js
+++ workers/fieldsexists.js
@@ -1,6 +1,4 @@
-/**
- * Checks for the existence of the specified property and returns the entire object if the property is exists
- */
+
 export default (options,data,req,res) => {
 	for(let i=0;i<options.fields.length;i++){
 		const f = options.fields[i]
